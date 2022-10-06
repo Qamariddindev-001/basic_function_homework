6bb9befdcf670919abf167e37d09115b63f2fc3d
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-06 18:48:20 +0500

    bin/basic_func02.dart

--- bin/basic_func02.dart
+++ bin/basic_func02.dart
@@ -1,4 +1,11 @@
-// Create a function called divide. 
-//Create function arguments a and b of type int. 
+// Create a function called divide.
+//Create function arguments a and b of type int.
 //Returns the floor division of a and b.
-void main() {}
+double divide(int a, b) {
+  double summ = a / b;
+  return summ;
+}
+
+void main() {
+  print(divide(10, 8));
+}
