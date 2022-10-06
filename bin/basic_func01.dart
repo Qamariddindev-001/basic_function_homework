8429e3d461800f30d86021c07a47ca540bdf1605
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-06 18:23:16 +0500

    bin/basic_func01.dart

--- bin/basic_func01.dart
+++ bin/basic_func01.dart
@@ -1,4 +1,11 @@
 // Create a function called func.
 // Create two arguments type int
 //  Return the summ two arguments
-void main() {}
+int func(int a, b) {
+  int summ = a + b;
+  return summ;
+}
+
+void main() {
+  print(func(6, 7));
+}
