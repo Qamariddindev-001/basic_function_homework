f6756a7bc46490c43f5978c914c61604cbd3ea02
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-11 23:09:01 +0500

    bin/basic_func05.darttuzildi

--- bin/basic_func05.dart
+++ bin/basic_func05.dart
@@ -1,4 +1,18 @@
 // Create a function called func.
 // Create a fucntion argument ‘number’ two digits numbers will be given.
 // return the sum of the number’s digits.
-void main() {}
+
+int func(
+  int n,
+) {
+  int x1, x2;
+
+  x1 = n % 100 ~/ 10;
+  x2 = n % 100 % 10;
+  int ans = x1 + x2;
+  return ans;
+}
+
+void main() {
+  print(func(32));
+}
