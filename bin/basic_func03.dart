7a987007d840e6e032690c90c15bbc1fe1d0da4f
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-11 22:14:44 +0500

    bin/basic_func03.dart

--- bin/basic_func03.dart
+++ bin/basic_func03.dart
@@ -1,4 +1,15 @@
-//Create a function called func. 
-//Create function arguments a and b of type int. 
+//Create a function called func.
+//Create function arguments a and b of type int.
 //Returns the remainder of a and b.
-void main() {}
+double func(
+  int a,
+  int b,
+) {
+  double ans = a / b;
+
+  return ans;
+}
+
+void main() {
+  print(func(55, 4));
+}
